39b7d0abe893664565569621cd6de8b2ac598750
sweenejp <sweenejp@gmail.com>
2022-06-11 10:12:36 -0500

    update About section

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function About() {
   return (
-    <div className='about'>
-      <p className='section__description'>
-        Hi there! I am a front end web developer and high school science
-        teacher. Feel free to explore my portfolio. You can reach out to me by
-        visiting the contact section below.
+    <div className="about">
+      <p className="section__description">
+        Hi there! I am a professional front end web developer. Feel free to
+        explore my portfolio. You can reach out to me by visiting the contact
+        section below.
       </p>
     </div>
   );
